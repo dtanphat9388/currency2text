f1bcbd21e6df39b3bb257d9052e22c694660a76a
dtanphat9388 <dtanphat9388@gmail.com>
2018-11-21 17:39:18 +0700

    replace toLocaleString 	toLocaleString limited to 16 digits

--- client.js
+++ client.js
@@ -6,21 +6,33 @@ const input = document.querySelector('.input')
 const value = document.querySelector('.value')
 
 value.oninput = function(e) {
-  this.value !== 0 ? input.classList.remove("empty")
-  : input.classList.add("empty")
+  this.value !== 0
+    ? input.classList.remove("empty")
+    : input.classList.add("empty")
   
   let rawValue = this.value.replace(/,/g, "")
   
   if (rawValue.length > 18) {
     rawValue = rawValue.slice(0, 18)
   }
-                    
+
   result.textContent = convert(rawValue);
 
-  const thousand = (+rawValue).toLocaleString('vn')
-  this.value = thousand
+  if (rawValue.length > 3) {
+    return this.value = formated(rawValue)
+  }
 }
 
 value.onkeypress = function(evt) {
   if (evt.key === "Enter") evt.preventDefault();
+}
+
+function formated(rawValue) {
+  const separator = rawValue.replace(/(\d{3})+$/g, ',$&');
+  let [left, right] = separator.split(',');
+  right = right.match(/\d{3}/g)
+  if (rawValue.length % 3 === 0 && right.length > 1) {
+    return _.join(right, ',')
+  }
+  return `${left},${_.join(right, ",")}`
 }
