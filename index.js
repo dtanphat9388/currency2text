6a3b4a8dc09c39035bf564237141e424aaadc36e
Duong Tan Phat <phat.duong@vietan-software.com>
2020-06-21 01:05:44 +0700

    optimize code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /* 
-              ty  trieu ngan dong
-    number : 123   456   789 123
-    minor  : abc   abc   abc abc
-    major  : xxx   xxx   xxx xxx 
+    unit   :  ty trieu ngan dong
+    number : 123   456  789  123
+    minor  : abc   abc  abc  abc
+    major  : xxx   xxx  xxx  xxx 
  */
 
 /** WORKFLOW
- *   number:                 103 056
+ *   input:                  103 056
  *   fill 0:         000 000 103 056
  *   remove 0:         0   0 103  56
  *   replace 0 = "":  ""  "" 103  56 => handleText
@@ -14,7 +14,7 @@
  */
 
 /** CASE
- *  2 số:  0-9    => không - chính
+ *  2 số:  0-9    => không - chín
  *         10     => mười
  *         1x     => mười <x>
  *         xx     => <x> mươi <x>
@@ -23,34 +23,45 @@
  */
 const _ = require('lodash')
 
-const blocks = ['triệu', 'ngàn', 'tỷ', 'triệu', 'ngàn'];
-const majorLimit = 18
-
 const defaultOptions = {
   unit: 'đồng',
+  blocks: ['triệu', 'ngàn', 'tỷ', 'triệu', 'ngàn'],
+  majorLimit: 18
 }
-function convert(number, options = defaultOptions) {
-  if (typeof number !== 'number') number = +number;
-  const _number = `${number}`.slice(-majorLimit); // limited to 18 digits
-  if (!/^\d+$/g.test(_number)) throw new Error("Argument have to a number!");
 
-  switch (_number.length) {
-    case 1: case 2: case 3: return `${handleDigitToStr(number)} ${options.unit}`
+/**
+ * @param {string|number} number 
+ * @param {object} options 
+ */
+function main(number = 0, options = defaultOptions) {
+  try {
+    let _number = `${+number}`
+
+    if (!/^\d+$/g.test(_number)) return "Số không hợp lệ";
+
+    _number = _number.slice(-options.majorLimit); // limited to 18 digits
+
+    switch (_number.length) {
+      case 1: case 2: case 3: return `${handleDigitToStr(number)} ${options.unit}`
+    }
+
+    const { unit } = options;
+    options.blocks[5] || options.blocks.push(unit)
+
+    return handleMajor(number, options.blocks, options)
   }
-
-  const { unit } = options;
-  blocks[5] || blocks.push(unit)
-
-  const major = fillNumber(_number, majorLimit)
-  return handleMajor(major, blocks, options)
+  catch (err) {
+    return err.message
+  }
 }
 
 
 function handleMajor(major, blocks, options) {
+  major = fillNumber(major, options.majorLimit)
   const minorsString = major.match(/\d{3}/g)
   const minorsNumber = _.map(minorsString, (minor) => +minor) // remove 0 trong mỗi minor block
 
-  const minorsMerge = _.zipWith(minorsNumber, blocks, (number, block) => ({ block, number }))
+  const minorsMerge = _.zipWith(minorsNumber, blocks, (number, block) => ({ number, block }))
   const minorsCompacted = _.dropWhile(minorsMerge, item => item.number == 0)
 
   return _.reduce(minorsCompacted, (prev, curr, index, list) => {
@@ -93,28 +104,34 @@ function handleDigitToStr(number) {
   }
 }
 
-const numberStr = ["không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín", "mười"]
+const numberStr = ["không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"]
 function oneDigitToStr(number) {
   return numberStr[number]
 }
 
 function twoDigitToStr(number) {
-  let chuc = parseInt(number / 10);  // 25 => 2                              
-  let donvi = number % 10;           // 25 => 5
-  if (number == 10) return oneDigitToStr(number);                         // 10
-  if (chuc !== 1 && donvi === 0) return `${oneDigitToStr(chuc)} mươi`;    // 20, 30, ..., 90
-  if (chuc === 1 && donvi !== 0) return `muời ${oneDigitToStr(donvi)}`;   // 1x
-  return `${oneDigitToStr(chuc)} mươi ${oneDigitToStr(donvi)}`;           // xx
+  const chuc = parseInt(number / 10);  // 25 => 2                              
+  const donvi = number % 10;           // 25 => 5
+
+  const text_chuc = oneDigitToStr(chuc)
+  const text_donvi = oneDigitToStr(donvi)
+
+  if (chuc === 1 && donvi === 0) return "mười";                   // 10
+  if (chuc === 1 && donvi !== 0) return `muời ${text_chuc}`;      // 1x
+  if (chuc !== 1 && donvi === 0) return `${text_donvi} mươi`;     // 20, 30, ..., 90
+  return `${text_chuc} mươi ${text_donvi}`;                       // xx
 }
 
 function threeDigitToStr(number) {
-  let tram = parseInt(number / 100); // 325 => 3
-  let chuc = number % 100;           // 325 => 25
-  if (chuc === 0) return `${oneDigitToStr(tram)} trăm`;
-  if (chuc < 10 && chuc > 0) chuc = `lẻ ${oneDigitToStr(chuc)}`;
-  else { chuc = twoDigitToStr(chuc) };
-  return `${oneDigitToStr(tram)} trăm ${chuc}`
+  const tram = parseInt(number / 100); // 325 => 3
+  const chuc = number % 100;           // 325 => 25
+
+  const text_tram = oneDigitToStr(tram)
+  const text_chuc = chuc < 10 && chuc > 0 ? `lẻ ${oneDigitToStr(chuc)}` : twoDigitToStr(chuc)
+
+  if (chuc === 0) return `${text_tram} trăm`;
+  return `${text_tram} trăm ${text_chuc}`
 }
 
-
-module.exports = convert
+console.log(main("1535432"))
+module.exports = main
