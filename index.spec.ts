fb27a2d2c685bbecd2f9a35bf6a85bc2d31ddda3
dtanphat9388 <dtanphat9388@gmail.com>
2021-10-10 14:46:53 +0700

    refactor testcases

--- index.spec.ts
+++ index.spec.ts
@@ -11,81 +11,112 @@ import cur2text,
   } 
 from './index'
 
-test("Should be correct text with one number", () => {
-  expect(handleOneNumberToString("0")).toBe("không")
-  expect(handleOneNumberToString("1")).toBe("một")
-  expect(handleOneNumberToString("2")).toBe("hai")
-  expect(handleOneNumberToString("3")).toBe("ba")
-  expect(handleOneNumberToString("4")).toBe("bốn")
-  expect(handleOneNumberToString("5")).toBe("năm")
-  expect(handleOneNumberToString("6")).toBe("sáu")
-  expect(handleOneNumberToString("7")).toBe("bảy")
-  expect(handleOneNumberToString("8")).toBe("tám")
-  expect(handleOneNumberToString("9")).toBe("chín")
+describe("handleOneNumberToString(): Should return text with one digit", () => {
+  test.each([
+    {input: "0", result: "không"},
+    {input: "1", result: "một"},
+    {input: "2", result: "hai"},
+    {input: "3", result: "ba"},
+    {input: "4", result: "bốn"},
+    {input: "5", result: "năm"},
+    {input: "6", result: "sáu"},
+    {input: "7", result: "bảy"},
+    {input: "8", result: "tám"},
+    {input: "9", result: "chín"},
+  ])("Should $input tobe $result", ({input, result}) => {
+    expect(handleOneNumberToString(input)).toBe(result)
+  })
 })
 
-test("Should be correct text with two number", () => {
-  expect(handleTwoNumberToString("00")).toBe("không")
-  expect(handleTwoNumberToString("10")).toBe("mười")
-  expect(handleTwoNumberToString("11")).toBe("mười một")
-  expect(handleTwoNumberToString("20")).toBe("hai mươi")
-  expect(handleTwoNumberToString("34")).toBe("ba mươi bốn")
+describe("handleTwoNumberToString(): Should return text with two digits", () => {
+  test.each([
+    {input: "00", result: "không"},
+    {input: "10", result: "mười"},
+    {input: "11", result: "mười một"},
+    {input: "20", result: "hai mươi"},
+    {input: "34", result: "ba mươi bốn"},
+  ])("Should $input tobe $result", ({input, result}) => {
+    expect(handleTwoNumberToString(input)).toBe(result)
+  })
 })
 
-test("Should be correct text with three number", () => {
-  expect(handleThreeNumberToString("000")).toBe("không")
-  expect(handleThreeNumberToString("100")).toBe("một trăm")
-  expect(handleThreeNumberToString("101")).toBe("một trăm lẻ một")
+describe("handleThreeNumberToString(): Should return text with three digits", () => {
+  test.each([
+    {input: "000", result: "không"},
+    {input: "100", result: "một trăm"},
+    {input: "101", result: "một trăm lẻ một"},
+  ])("Should $input tobe $result", ({input, result}) => {
+    expect(handleThreeNumberToString(input)).toBe(result)
+  })
 })
 
-test("Should corrent input with thousand separators", () => {
-  expect(removeThousandSeparator("00-1 23-4 56_7 89.000")).toBe("123456789000")
+describe("validate input", () => {
+  test("Should accept input with thousand separators", () => {
+    expect(removeThousandSeparator("00-1 23-4 56_7 89.000")).toBe("123456789000")
+  })
+  
+  test.each([
+    {input: '594325', result: true,  desc: "is all number"},
+    {input: '54a325', result: false, desc:  "have a 'a' character"},
+    {input: '54A325', result: false, desc:  "have a 'A' character"},
+  ])('Should input: $input \t toBe $result \t because $desc', ({input, result}) => {
+    expect(checkValid(input)).toBe(result)
+  })
 })
 
-test("Should be input is all number", () => {
-  expect(checkValid('54325')).toBe(true)
-  expect(checkValid('54a325')).toBe(false)
-  expect(checkValid('54A325')).toBe(false)
-  expect(checkValid('54 325')).toBe(false)
-  expect(checkValid('54-325')).toBe(false)
-  expect(checkValid('54.325')).toBe(false)
+test.each([
+  {input: "0", result: "000"},
+  {input: "00", result: "000"},
+  {input: "00", result: "000"},
+  {input: "0000", result: "000000"},
+  {input: "0000000", result: "000000000"},
+])("Should $input tobe $result", ({input, result}) => {
+  expect(addPadding(input)).toBe(result)
 })
 
-test("Should addPadding to input", () => {
-  expect(addPadding("0")).toBe("000")
-  expect(addPadding("00")).toBe("000")
-  expect(addPadding("000")).toBe("000")
-  expect(addPadding("0000")).toBe("000000")
-  expect(addPadding("0000000")).toBe("000000000")
+describe('Should not handle if input empty', () => {
+  test.each([
+    {input: "0", result: ""},
+    {input: "00", result: ""},
+    {input: "000", result: ""},
+  ])("expect $input tobe $result", ({input, result}) => {
+    expect(cur2text(input)).toBe(result)
+  })
 })
 
-test("Should not handle if input empty", () => {
-  expect(cur2text("0")).toBe("")
-  expect(cur2text("00")).toBe("")
-  expect(cur2text("000")).toBe("")
-})
-
-test("Suffix là 'chẵn' nếu last block === 000", () => {
-  expect(cur2text("1000")).toMatch(/.*chẵn$/)
-  expect(cur2text("1001")).toMatch(/.*đồng$/)
+describe('Should text context by last block', () => {
+  test.each([
+    {input: "1000", result: /.*chẵn$/, desc: "entity last block is 0"},
+    {input: "1001", result: /.*đồng$/, desc: "any digit in last block not 0"},
+  ])('expect $input tobe $result because $desc', ({input, result}) => {
+    expect(cur2text(input)).toMatch(result)
+  })
 })
 
 test("Should be handle if block length <= 3", () => {
   expect(cur2text("100")).toBe("một trăm đồng")
 })
 
-test("Should be correct currency string", () => {
-  expect(cur2text("")).toBe("")
-  expect(cur2text("0")).toBe("")
-  expect(cur2text("1000")).toBe("một ngàn chẵn")
-  expect(cur2text("24000")).toBe("hai mươi bốn ngàn chẵn")
-  expect(cur2text("106211")).toBe("một trăm lẻ sáu ngàn hai trăm mười một đồng")
-  expect(cur2text("5106211")).toBe("năm triệu một trăm lẻ sáu ngàn hai trăm mười một đồng")
-  expect(cur2text("1505106211")).toBe("một tỷ năm trăm lẻ năm triệu một trăm lẻ sáu ngàn hai trăm mười một đồng")
-  expect(cur2text("1505000211")).toBe("một tỷ năm trăm lẻ năm triệu hai trăm mười một đồng")
+describe('Should get correct currency string with input number', () => {
+  test.each([
+    {input: "", result: ""},
+    {input: "0", result: ""},
+    {input: "1000", result: "một ngàn chẵn"},
+    {input: "24000", result: "hai mươi bốn ngàn chẵn"},
+    {input: "106211", result: "một trăm lẻ sáu ngàn hai trăm mười một đồng"},
+    {input: "5106211", result: "năm triệu một trăm lẻ sáu ngàn hai trăm mười một đồng"},
+    {input: "1505106211", result: "một tỷ năm trăm lẻ năm triệu một trăm lẻ sáu ngàn hai trăm mười một đồng"},
+    {input: "1505000211", result: "một tỷ năm trăm lẻ năm triệu hai trăm mười một đồng"},
+  ])("expect $input tobe $result", ({input, result}) => {
+    expect(cur2text(input)).toBe(result)
+  })
 })
 
-test("Shout be break input to three number blocks", () => {
-  expect(breakInputToBlocks("456")).toMatchObject(["456"])
-  expect(breakInputToBlocks("123456")).toMatchObject(["123","456"])
+describe("Shout be break input to three number blocks", () => {
+  test.each([
+    {input: "456", result: ["456"], desc: ""},
+    {input: "123456789", result: ["123", '456', '789'], desc: ""},
+  ])('expect $input tobe $result because $desc', ({input, result}) => {
+    expect(breakInputToBlocks(input)).toMatchObject(result)
+  })
 })
